83102a7de7c9bb82a42d8448f7819c466460d915
宋治佳 <jia@songzhj.com>
2016-04-25 10:48:14 +0800

    update

--- Stage 2/Task19/index.js
+++ Stage 2/Task19/index.js
@@ -1,10 +1,32 @@
+//柱形图宽度
+var position = {width: 0, i: 0};
+//循环颜色的数组
+colors = ['#F70808', '#6B04F6', '#504C4C', '#2BF60A','#000000', '#FFFC00'];
+
+/**
+ * 获取并判断输入数据
+ */
+function getInputValue() {
+ 	var input = document.getElementById('input');
+ 	var value = input.value;
+ 	if (value >= 10 && value <= 100 && document.getElementById('wrap').childElementCount < 60) {
+ 		return value;
+ 	} else {
+ 		alert("输入的数据不符合范围");
+ 		return;
+ 	}
+ }
+
 /**
   * 左侧入
  */
 function addElementLeft() {
 	var wrap = document.getElementById('wrap');
-	var input = document.getElementById('input');
-	var html = "<div class='item'>" + input.value + "</div>";
+	var value = getInputValue();
+	if (value === undefined) {
+		return;
+	}
+	var html = "<div class='item' data-num='"+ value +"' style='height:"+ value * 2 + "px;width:" + position.width + "px;background-color:" + colors[position.i++ % colors.length] + ";'></div>";
 	wrap.innerHTML = html + wrap.innerHTML;
 }
 
@@ -13,8 +35,24 @@ function addElementLeft() {
  */
 function addElementRight() {
 	var wrap = document.getElementById('wrap');
-	var input = document.getElementById('input');
-	var html = "<div class='item'>" + input.value + "</div>";
+	var value = getInputValue();
+	if (value == undefined) {
+		return;
+	}
+	var html = "<div class='item' data-num='"+ value +"' style='height:"+ value * 2 + "px;width:" + position.width + "px;background-color:" + colors[position.i++ % colors.length] + ";'></div>";
+	wrap.innerHTML += html;
+}
+
+/**
+ * 随机产生25组数据
+*/
+function numberFactory() {
+	var wrap = document.getElementById('wrap');
+	var html = '';
+	for (var i = 0; i < 25; ++i) {
+		var value = Math.floor(Math.random() * 90 + 10);
+		html += "<div class='item' data-num='"+ value +"' style='height:"+ value * 2 + "px;width:" + position.width + "px;background-color:" + colors[position.i++ % colors.length] + ";'></div>";
+	}
 	wrap.innerHTML += html;
 }
 
@@ -35,22 +73,73 @@ function removeElementRight() {
 }
 
 /**
-  * 右侧删除
+  * 点击删除
  */
 function removeElementByClick(event) {
 	var wrap = document.getElementById('wrap');
 	var p = wrap.firstElementChild;
-	for (var i = 0; i < wrap.childElementCount - 1; ++i, p = p.nextElementSibling) {
+	for (var i = 0; i < wrap.childElementCount; ++i, p = p.nextElementSibling) {
 		if (p == event.target) {
 			wrap.removeChild(p);
+			break;
 		}
 	}
 }
 
+/**
+ * 排序演示
+ */
+function sortDemo() {
+	var wrap = document.getElementById('wrap');
+	var p = wrap.firstElementChild;
+	var q = wrap.firstElementChild.nextElementSibling;
+	var i = 0, j = 0;
+	var sort = function() {
+		var num1 = p.getAttribute('data-num');
+		var num2 = q.getAttribute('data-num');
+		q.style.border = '2px solid #999';
+		p.style.border = '';
+		if (num1 > num2) {
+			var temp = {height:p.style.height, backgroundColor:p.style.backgroundColor};
+			p.style.height = q.style.height;
+			p.style.backgroundColor = q.style.backgroundColor;
+			p.setAttribute('data-num', num2);
+			q.style.height = temp.height;
+			q.style.backgroundColor = temp.backgroundColor;
+			q.setAttribute('data-num', num1);
+		}
+		p = p.nextElementSibling;
+		q = q.nextElementSibling;
+		++j;
+		if (j >= wrap.childElementCount - i - 1) {
+			p.style.border = '';
+			p = wrap.firstElementChild;
+			q = wrap.firstElementChild.nextElementSibling;		
+			++i;
+			j = 0;	
+		}
+		if (i >= wrap.childElementCount) {
+			clearInterval(t);
+		}
+	}
+	var t = setInterval(sort, 50);	
+}
+
+/**
+ * 初始化宽度
+*/
+function getPos() {
+	var wrap = document.getElementById('wrap');
+	position.width = Math.floor(wrap.clientWidth / 120);
+}
+
 (function() {
+	getPos();
 	document.getElementById('left-in').addEventListener('click', addElementLeft);
 	document.getElementById('right-in').addEventListener('click', addElementRight);
 	document.getElementById('left-out').addEventListener('click', removeElementLeft);
 	document.getElementById('right-out').addEventListener('click', removeElementRight);
 	document.getElementById('wrap').addEventListener('click', removeElementByClick);
+	document.getElementById('number-factory').addEventListener('click', numberFactory);
+	document.getElementById('sort').addEventListener('click', sortDemo);
 })();
